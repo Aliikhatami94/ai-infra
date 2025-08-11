3da2ae68b531cb6078096c5526188b13d851b828
Ali Khatami <aliikhatami94@gmail.com>
2025-08-11 17:43:45 -0400

    feat: ConditionalEdge can infer targets from router_fn if not provided; update usage example

--- src/ai_infra/graph/__init__.py
+++ src/ai_infra/graph/__init__.py
@@ -14,36 +14,39 @@ class State(TypedDict):
     value: int
     messages: list[str]  # for reducer demo
 
-def inc(state: State) -> State:
-    s = dict(state)
-    s["value"] += 1
-    return s
+if __name__ == '__main__':
 
-def times2(state: State) -> State:
-    s = dict(state)
-    s["value"] *= 2
-    return s
+    def inc(state: State) -> State:
+        s = dict(state)
+        s["value"] += 1
+        return s
 
-def over10(state: State) -> str:
-    # choose next node based on value
-    if state["value"] < 0:
-        return "inc"
-    if state["value"] < 10:
-        return "times2"
-    else:
-        return END
+    def times2(state: State) -> State:
+        s = dict(state)
+        s["value"] *= 2
+        return s
 
-math_graph = CoreGraph(
-    state_type=State,
-    node_definitions=[inc, times2],
-    edges=[
-        Edge(start="inc", end="times2"),
-        ConditionalEdge(
-            from_node="times2",
-            router_fn=over10,
-            targets=["inc", "times2", END],
-        ),
-    ]
-)
+    def over10(state: State) -> str:
+        # choose next node based on value
+        if state["value"] < 0:
+            return "inc"
+        if state["value"] < 8:
+            return "times2"
+        else:
+            return END
 
-print(math_graph.graph.get_graph().draw_mermaid())
+    math_graph = CoreGraph(
+        state_type=State,
+        node_definitions=[inc, times2],
+        edges=[
+            Edge(start="inc", end="times2"),
+            ConditionalEdge(
+                from_node="times2",
+                router_fn=over10,
+                targets=["inc", "times2", END],
+            ),
+        ]
+    )
+
+    res = math_graph.run({"value": 1, "messages": []})
+    print(res)
