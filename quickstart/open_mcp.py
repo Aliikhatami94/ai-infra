b7b8a893827790945607978f571857d22687afc2
Ali Khatami <aliikhatami94@gmail.com>
2025-08-16 11:52:54 -0400

    MCP ready for primetime

--- quickstart/open_mcp.py
+++ quickstart/open_mcp.py
@@ -1,31 +1,39 @@
 from src.ai_infra.open_mcp.core import CoreMCP
-from src.ai_infra.open_mcp.models import McpConfig, Server, ServerConfig, ServerMetadata, Prompt
+from src.ai_infra.open_mcp.models import McpConfig, Server, ServerConfig, ServerMetadata
 from ai_infra.llm import Models, Providers, CoreAgent
 
 # Example config following the models in src/ai_infra/mcp/models.py
-mcp_config = McpConfig(
+config = McpConfig(
     name="test_mcp",
     host="http://0.0.0.0:8000",
-    prompts=[
-        Prompt(contents=[
-            "Your name is Alex."
-        ])
-    ],
+    prompts=[],
     servers=[
+        Server(
+            metadata=ServerMetadata(
+                id="wikipedia_mcp",
+                name="wikipedia_mcp",
+                description="Wikipedia MCP server"
+            ),
+            config=ServerConfig(
+                command="npx",
+                args=["-y", "wikipedia-mcp"],
+                transport="stdio",
+            )
+        ),
         Server(
             metadata=ServerMetadata(
                 id="test-mcp",
                 name="test_server",
-                description="Test server",
+                description="Test server"
             ),
             config=ServerConfig(
                 url="/test-mcp/mcp",
                 transport="streamable_http",
-            ),
+            )
         )
-    ],
+    ]
 )
-mcp = CoreMCP(config=mcp_config)
+mcp = CoreMCP(config=config)
 
 # Example usage in an async context
 import asyncio
@@ -35,7 +43,7 @@ agent = CoreAgent()
 async def ask_agent():
     tools = await mcp.get_tools()
     res = await agent.arun_agent(
-        messages=[{"role": "user", "content": "How is the weather in Paris?"}],
+        messages=[{"role": "user", "content": "What is the capital of France? search wikipedia. then tell me the weather in New York City."}],
         provider=Providers.openai,
         model_name=Models.openai.gpt_4_1_mini.value,
         tools=tools
